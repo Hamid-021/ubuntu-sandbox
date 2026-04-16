bee4caadbc970b4dbbef060fafbcf5bd57546c49
Hameed <79967451+Hamid-021@users.noreply.github.com>
2026-04-16 18:03:40 +0530

    Modify the shellinabox service

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,6 +10,7 @@ shellinaboxd \
     --disable-ssl \
     --port=8080 \
     --service=/:"SSH:localhost:22" \
+    --ssh-args="-o RhostsRSAAuthentication=no -o RSAAuthentication=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
     --background
 
 echo "[entrypoint] Sandbox ready. Web terminal at http://0.0.0.0:8080"
